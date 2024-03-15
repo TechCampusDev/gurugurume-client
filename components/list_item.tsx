63407b4d0c62d518f1337a16a51600023739dcbf
liverHawk <hawkyofkn5365@gmail.com>
2024-03-15 23:02:14 +0900

    使っていないコンポーネントの削除

--- components/list_item.tsx
+++ components/list_item.tsx
@@ -1,5 +1,4 @@
-import {Button, StyleSheet, Text, View} from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import {StyleSheet, Text, View} from "react-native";
 import {useState} from "react";
 import {FontAwesome5} from "@expo/vector-icons"
 
