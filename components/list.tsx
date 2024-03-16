3d9a8eead2e9cf5f832fce38e58f9d948a81a4e9
liverHawk <hawkyofkn5365@gmail.com>
2024-03-16 17:52:52 +0900

    ダミーデータの追加

--- components/list.tsx
+++ components/list.tsx
@@ -29,8 +29,84 @@ export default function List() {
                     "id": 5,
                     "storeName": "スイーツハウスあまい",
                     "favorite": true
+                },
+                {
+                    "id": 6,
+                    "storeName": "ビストロ太陽",
+                    "favorite": false
+                },
+                {
+                    "id": 7,
+                    "storeName": "寿司処海山",
+                    "favorite": true
+                },
+                {
+                    "id": 8,
+                    "storeName": "バーベキューガーデン",
+                    "favorite": false
+                },
+                {
+                    "id": 9,
+                    "storeName": "サンドイッチステーション",
+                    "favorite": true
+                },
+                {
+                    "id": 10,
+                    "storeName": "カレーコーナー spice up",
+                    "favorite": false
+                },
+                {
+                    "id": 11,
+                    "storeName": "ヌードルショップ春雨",
+                    "favorite": true
+                },
+                {
+                    "id": 12,
+                    "storeName": "オーガニックサラダバー緑風",
+                    "favorite": false
+                },
+                {
+                    "id": 13,
+                    "storeName": "焼肉ハウス炎",
+                    "favorite": true
+                },
+                {
+                    "id": 14,
+                    "storeName": "鉄板ダイニング火花",
+                    "favorite": false
+                },
+                {
+                    "id": 15,
+                    "storeName": "タパス・バル風来",
+                    "favorite": true
+                },
+                {
+                    "id": 16,
+                    "storeName": "デザートカフェ甘露",
+                    "favorite": false
+                },
+                {
+                    "id": 17,
+                    "storeName": "ワインバー葡萄の樹",
+                    "favorite": true
+                },
+                {
+                    "id": 18,
+                    "storeName": "ラーメン激戦区",
+                    "favorite": false
+                },
+                {
+                    "id": 19,
+                    "storeName": "イタリアン・トラットリア海の幸",
+                    "favorite": true
+                },
+                {
+                    "id": 20,
+                    "storeName": "フレンチレストラン花椿",
+                    "favorite": false
                 }
             ]
+
         }
     };
 
